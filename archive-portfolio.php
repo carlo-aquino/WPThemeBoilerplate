aa6ba9d9844e85613c0c339748921bd45b4a77ea
carlo-aquino <carlopaulo.aquino@gmail.com>
2022-08-05 09:22:08 +0800

    Theme update ver 4.3.3

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -1,31 +1,14 @@
 <?php get_header(); ?>
 
-    <main class="archive-portfolio__main">
-
-        <?php get_template_part( 'template-parts/page', 'banner' ); ?>
+    <main class="portfolio-archive__main">
         
-        <section class="archive-portfolio mobile-spacer">
+        <?php
 
-            <div class="archive-portfolio__wrapper <?php if( !$full_width ) { echo 'content-limit'; } ?>">   
+            get_template_part( 'template-parts/page', 'banner' );
+            get_template_part( 'template-parts/blog', 'posts' );
+            
+        ?>
 
-                <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
-                
-                    <article>
-                        <h2><?php the_title(); ?></h2>
-                        <p><?php the_content(); ?></p>
-                        <img src="<?php the_field('school_logo'); ?>" alt="">
-                    </article>
-                    
-                <?php endwhile; endif; ?>    
-
-                <div class="page-content__search-pagination">
-                    <?php echo paginate_links(); ?>
-                </div>
-
-            </div>
-
-        </section>
-        
     </main>
 
 <?php get_footer(); ?>
--- single-portfolio.php
+++ single-portfolio.php
@@ -1,36 +1,15 @@
-<?php get_header();
+<?php get_header(); 
     $page_class = get_field( 'page_class' );
 ?>
 
-    <main class="single__portfolio<?php if( $page_class ) { echo ' ' . $page_class; } ?>">
-        <?php 
-            if( have_rows( 'page_banner' ) ) {
-                while( have_rows( 'page_banner' ) ){
-                    the_row(); 
-                    $page_bg_enable = get_sub_field( 'page_bg_enable' );
+    <main class="page__main<?php if( $page_class ) { echo ' ' . $page_class; } ?>">
 
-                    if( have_rows( 'banner_type' ) ) {
-                        while( have_rows( 'banner_type' ) ){
-                            the_row(); 
-
-                            if ( $page_bg_enable ) {
-                                get_template_part( 'template-parts/page', 'banner' );
-                            }
-                        }
-                    }
-                    
-                }  
-            } 
-        ?>
-
-        <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
-            
-            
-        <?php endwhile; endif; ?>
+        <?php get_template_part( 'template-parts/page', 'banner' ); ?>
 
         <div class="page-container">
             <?php get_template_part( 'template-parts/page', 'builder' ); ?>
         </div>
+        
     </main>
 
 <?php get_footer(); ?>
