3ea63318e9ccd4ddf61eff9a6b93444265479c20
carlo-aquino <carlopaulo.aquino@gmail.com>
2022-10-02 16:08:28 +0800

    [Update][Featured Slider Module] Allow multiple selection on featured slider module taxonomy

--- template-parts/acf-components/modules/featured_slider-module_post-content.php
+++ template-parts/acf-components/modules/featured_slider-module_post-content.php
@@ -9,6 +9,16 @@
         }
     }
 
+    if( $featured_slider_data_source_filter ) {
+        $results = array(); 
+        $ctr = 0;
+
+        while( $ctr < sizeof( $featured_slider_data_source_filter ) ) {
+            array_push( $results, $featured_slider_data_source_filter[$ctr]->slug );
+            $ctr++;
+        }
+    }
+
     $slider_post_query = new WP_Query(array(
         'post_type'             => $featured_slider_narrow_data_source->name,
         'posts_per_page'        => -1,
@@ -16,124 +26,126 @@
         'order'                 => 'ASC',
         'ignore_sticky_posts'   => 1,
 
-        // 'tax_query' => array(
-        //     array (
-        //         'taxonomy' => $featured_slider_data_source_filter,
-        //         'field' => 'slug',
-        //         'terms' => $featured_slider_data_source_filter,
-        //     )
-        // ),
+        'tax_query' => $featured_slider_data_source_filter ? array(
+            array (
+                'taxonomy' => $featured_slider_data_source_filter[0]->taxonomy,
+                'field' => 'slug',
+                'terms' => $results,
+            )
+        ) : '',
     )); 
 ?>
+    <?php if( $slider_post_query->have_posts() ):  ?>
+        <div class="featured-slider-module__container row g-0">
 
-    <div class="featured-slider-module__container row g-0">
+            <div class="featured-slider-module__wrapper swiper-wrapper--left">
+                
+                <div class="featured-slider-module__cards featured-slider__left slider-left" data-aos="fade-right" data-aos-duration="800">
 
-        <div class="featured-slider-module__wrapper swiper-wrapper--left">
-            
-            <div class="featured-slider-module__cards featured-slider__left slider-left" data-aos="fade-right" data-aos-duration="800">
+                    <div class="featured-slider-module__cards-wrapper swiper-wrapper">
+                        
+                        <?php while( $slider_post_query->have_posts() ): $slider_post_query->the_post(); ?>
+                            <article class="featured-slider-module__cards__card swiper-slide">
+                                
+                                <header class="featured-slider-module__cards__card-content">
+                                    <h2><?php the_title(); ?></h2>
+                                </header>
 
-                <div class="featured-slider-module__cards-wrapper swiper-wrapper">
-                    
-                    <?php if( $slider_post_query->have_posts() ): while( $slider_post_query->have_posts() ): $slider_post_query->the_post(); ?>
-                        <article class="featured-slider-module__cards__card swiper-slide">
-                            
-                            <header class="featured-slider-module__cards__card-content">
-                                <h2><?php the_title(); ?></h2>
-                            </header>
+                                <?php the_excerpt(); ?>
 
-                            <?php the_excerpt(); ?>
+                                <div class="featured-slider-module__cards__card-buttons">
+                                    <div class="button-module">
+                                        <div class="button-module__wrapper">
+                                            
+                                            <a href="<?php the_permalink(); ?>">
+                                                <span class="custom-primary-btn">More Info</span>
+                                            </a> 
 
-                            <div class="featured-slider-module__cards__card-buttons">
-                                <div class="button-module">
-                                    <div class="button-module__wrapper">
-                                        
-                                        <a href="<?php the_permalink(); ?>">
-                                            <span class="custom-primary-btn">More Info</span>
-                                        </a> 
-
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                        </article>
-                    <?php endwhile; endif; wp_reset_postdata(); ?>
+                            </article>
+                        <?php endwhile; wp_reset_postdata(); ?>
 
-                </div>    
+                    </div>    
 
-            </div>       
-        </div>
+                </div>       
+            </div>
 
-        <div class="featured-slider-module__wrapper swiper-wrapper--right">
-            
-            <div class="featured-slider-module__cards featured-slider__right slider-right"<?php if( $featured_slider_height ){ echo ' style="height: ' . $featured_slider_height . 'rem";'; } ?> data-aos="slide-left" data-aos-duration="1000" data-aos-easing="ease-out-sine">
+            <div class="featured-slider-module__wrapper swiper-wrapper--right">
+                
+                <div class="featured-slider-module__cards featured-slider__right slider-right"<?php if( $featured_slider_height ){ echo ' style="height: ' . $featured_slider_height . 'rem";'; } ?> data-aos="slide-left" data-aos-duration="1000" data-aos-easing="ease-out-sine">
 
-                <div class="featured-slider-module__cards-wrapper swiper-wrapper">
-                    <?php if( $slider_post_query->have_posts() ): while( $slider_post_query->have_posts() ): $slider_post_query->the_post(); ?>
+                    <div class="featured-slider-module__cards-wrapper swiper-wrapper">
+                        <?php while( $slider_post_query->have_posts() ): $slider_post_query->the_post(); ?>
 
-                        <article class="featured-slider-module__cards__card swiper-slide column-center">
-                                
-                            <div class="featured-slider-module__cards__card-overlay background-overlay"></div>
-                            
-                            <?php the_post_thumbnail( 'theme-small',
-                                array(
-                                    'class' => 'img-fluid',
-                                    'width' => 640,
-                                    'alt'   => get_the_title(),
-                                ) 
-                            ); ?>
-
-                        </article>
-
-                    <?php endwhile; endif; wp_reset_postdata(); ?>
-                </div>
-
-                <div class="featured-slider-module__cards-overlay cards-overlay"></div>
-
-            </div> 
-
-            <div class="swiper-button-prev featured-slider-module__arrow-prev" data-aos="custom-fade" data-aos-duration="1000" data-aos-delay="200" data-aos-easing="ease-out-sine"></div>   
-            <div class="swiper-button-next featured-slider-module__arrow-next" data-aos="custom-fade" data-aos-duration="1000" data-aos-delay="200" data-aos-easing="ease-out-sine"></div>   
-        </div>
-
-    </div>
-
-    <div class="featured-slider-module__container-mobile">
-
-        <div class="featured-slider-module__wrapper">
-            
-            <div class="featured-slider-module__cards slider-mobile">
-
-                <div class="featured-slider-module__cards-wrapper swiper-wrapper">
-                    <?php if( $slider_post_query->have_posts() ): while( $slider_post_query->have_posts() ): $slider_post_query->the_post(); ?>
-                    
-                        <article class="featured-slider-module__cards__card swiper-slide column-center">
+                            <article class="featured-slider-module__cards__card swiper-slide column-center">
                                     
-                            <a href="<?php the_permalink(); ?>"><span class="hit-area"></span></a>
+                                <div class="featured-slider-module__cards__card-overlay background-overlay"></div>
+                                
+                                <?php the_post_thumbnail( 'theme-small',
+                                    array(
+                                        'class' => 'img-fluid',
+                                        'width' => 640,
+                                        'alt'   => get_the_title(),
+                                    ) 
+                                ); ?>
 
-                            <div class="featured-slider-module__cards__card-overlay background-overlay"></div>
+                            </article>
 
-                            <div class="featured-slider-module__cards__card-heading title-overlay">
-                                <h2><?php the_title(); ?></h2>
-                            </div>
-                            
-                            <?php the_post_thumbnail( 'theme-xsmall',
-                                array(
-                                    'class' => 'img-fluid',
-                                    'width' => 640,
-                                    'alt'   => get_the_title(),
-                                ) 
-                            ); ?>
+                        <?php endwhile; wp_reset_postdata(); ?>
+                    </div>
 
-                        </article>
+                    <div class="featured-slider-module__cards-overlay cards-overlay"></div>
 
-                    <?php endwhile; endif; wp_reset_postdata(); ?>
-                </div>
+                </div> 
 
-            </div>      
+                <div class="swiper-button-prev featured-slider-module__arrow-prev" data-aos="custom-fade" data-aos-duration="1000" data-aos-delay="200" data-aos-easing="ease-out-sine"></div>   
+                <div class="swiper-button-next featured-slider-module__arrow-next" data-aos="custom-fade" data-aos-duration="1000" data-aos-delay="200" data-aos-easing="ease-out-sine"></div>   
+            </div>
+
+        </div>
+
+        <div class="featured-slider-module__container-mobile">
+
+            <div class="featured-slider-module__wrapper">
+                
+                <div class="featured-slider-module__cards slider-mobile">
+
+                    <div class="featured-slider-module__cards-wrapper swiper-wrapper">
+                        <?php while( $slider_post_query->have_posts() ): $slider_post_query->the_post(); ?>
+                        
+                            <article class="featured-slider-module__cards__card swiper-slide column-center">
+                                        
+                                <a href="<?php the_permalink(); ?>"><span class="hit-area"></span></a>
+
+                                <div class="featured-slider-module__cards__card-overlay background-overlay"></div>
+
+                                <div class="featured-slider-module__cards__card-heading title-overlay">
+                                    <h2><?php the_title(); ?></h2>
+                                </div>
+                                
+                                <?php the_post_thumbnail( 'theme-xsmall',
+                                    array(
+                                        'class' => 'img-fluid',
+                                        'width' => 640,
+                                        'alt'   => get_the_title(),
+                                    ) 
+                                ); ?>
+
+                            </article>
+
+                        <?php endwhile; wp_reset_postdata(); ?>
+                    </div>
+
+                </div>      
+                
+            </div>
             
         </div>
-        
-    </div>
+
+    <?php endif; ?>
 
 <script type="module">
     let featuredSliderLeft = new Swiper('.featured-slider__left', {
