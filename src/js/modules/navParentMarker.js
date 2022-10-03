7e89629da76dae5c2d15883d6abebfe62b2b0eda
carlo-aquino <carlopaulo.aquino@gmail.com>
2022-10-04 00:55:46 +0800

    [Fix][Header] script cleanup

--- src/js/modules/navParentMarker.js
+++ src/js/modules/navParentMarker.js
@@ -5,21 +5,21 @@ const navMarker = ()=> {
     const mobile_menu_child = document.querySelectorAll( '#menu-header-menu-1 .sub-menu' );
     const mobile_menu_child_child = document.querySelectorAll( '#menu-header-menu-1 .sub-menu .sub-menu' );
 
-    for( let i=0; i<menu_child.length; i++ ) {
-        menu_child[i].parentElement.classList.add( 'add-icon' );
-    }
+    menu_child.forEach( menu => {
+        menu.parentElement.classList.add( 'add-icon' );
+    } );
 
-    for( let j=0; j<menu_child_child.length; j++ ) {
-        menu_child_child[j].parentElement.classList.add( 'add-icon-02' );
-    }
+    menu_child_child.forEach( menu => {
+        menu.parentElement.classList.add( 'add-icon-02' );
+    } );
 
-    for( let k=0; k<mobile_menu_child.length; k++ ) {
-        mobile_menu_child[k].parentElement.classList.add( 'add-icon-03' );
-    }
+    mobile_menu_child.forEach( menu => {
+        menu.parentElement.classList.add( 'add-icon-03' );
+    } );
 
-    for( let l=0; l<mobile_menu_child_child.length; l++ ) {
-        mobile_menu_child_child[l].parentElement.classList.add( 'add-icon-03' );
-    }
+    mobile_menu_child_child.forEach( menu => {
+        menu.parentElement.classList.add( 'add-icon-03' );
+    } );
 }
 
 export default navMarker();
