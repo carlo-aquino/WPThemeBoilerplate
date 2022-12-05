85224109a2f79986922d8d91d33c8fdcbd91fdf8
carlo-aquino <carlopaulo.aquino@gmail.com>
2022-12-05 14:13:35 +0800

    [Fix][Header] fix another layout on mobile responsive

--- src/js/modules/mobileMenu.js
+++ src/js/modules/mobileMenu.js
@@ -1,20 +1,19 @@
 const mobileMenu = ()=> {
+    const root_doc = document.querySelector('body');
     const mob_icon_menu = document.querySelector('#mobile-header__icon-menu');
     const mob_drawer_close = document.querySelector('.mobile-header__drawer-overlay');
     const mob_drawer = document.querySelector('#mobile-header__drawer');
 
-    mob_drawer.addEventListener('touchmove', function(e) {
-        e.preventDefault();
-    }, {passive: false});
-
     mob_icon_menu.addEventListener("click", function() {
         mob_drawer.classList.toggle("mobile-header__drawer--animation");
         mob_drawer_close.classList.toggle("mobile-header__drawer--fade");
+        root_doc.classList.toggle("overflow-hidden");
     });
 
     mob_drawer_close.addEventListener("click", function() {
         mob_drawer.classList.toggle("mobile-header__drawer--animation");
         mob_drawer_close.classList.toggle("mobile-header__drawer--fade");
+        root_doc.classList.toggle("overflow-hidden");
     });
 }
 
